039cb8aeabf3ce94aef02a21191b0a50ed9e567d
Darren Ethier <darren@roughsmootheng.in>
2015-02-06 09:01:14 -0500

    Create de_ee_session-garbage-collection.php

---
+++ utility/de_ee_session-garbage-collection.php
@@ -0,0 +1,8 @@
+//you could add this as a callback on a wp_cron schedule, or somewhere else.
+public function de_ee_transient_garbage_collection() {
+		global $wpdb;
+
+		$now = time();
+		$query = "DELETE FROM t1, t2 USING $wpdb->options AS t1 JOIN $wpdb->options AS t2 ON t2.option_name = replace( t1.option_name, '_timeout', '' )  WHERE ( t1.option_value - 86400 ) < $now AND t1.option_name LIKE '\_transient\_timeout\_ee\_ssn\_%'";
+		$wpdb->query( $query );
+	}
