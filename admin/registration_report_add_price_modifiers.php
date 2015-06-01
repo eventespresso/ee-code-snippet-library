7cb942aa2abfc80dd6910604ec1b25700aef852c
Michael Nelson <michael@eventespresso.com>
2015-06-01 12:52:03 -0700

    added snippet for modifying a registration reports

---
+++ admin/registration_report_add_price_modifiers.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Use this filter with EE4.6+ to add another column onto the registration CSV report
+ * that shows the price modifiers for each registration's ticket purchased
+ */
+
+add_filter( 'FHEE__EE_Export__report_registrations__reg_csv_array', 'espresso_add_ticket_base_price', 10, 2);
+function espresso_add_ticket_base_price( $reg_csv_array, $reg_row ) {
+
+	//want the line item that's a child of this registration's ticket's line item
+	$line_item_id_for_reg = EEM_Line_Item::instance()->get_var(
+			array(
+				array(
+					'Ticket.TKT_ID' => $reg_row['Registration.TKT_ID']
+				)
+			));
+	$sub_line_items = EEM_Line_Item::instance()->get_all(
+			array(
+				array(
+					'LIN_parent' => $line_item_id_for_reg,
+					'LIN_type' => EEM_Line_Item::type_sub_line_item
+				),
+				'order_by' => array( 'LIN_order' => 'asc' )
+			));
+
+	$sub_line_item_details = array();
+	foreach( $sub_line_items as $sub_line_item ) {
+		$sub_line_item_details[] = sprintf( __( '%1$s %2$s', 'event_espresso' ), $sub_line_item->name(), $sub_line_item->get_pretty( 'LIN_total', 'localized_float' ) );
+	}
+
+	$reg_csv_array[ __( 'Ticket Price Breakdown', 'event_espresso' ) ] = implode('+', $sub_line_item_details );
+	return $reg_csv_array;
+}
