c8a2eb47ee360e7834fead27259015b197b3ef4a
Josh Feck <josh@nxnweb.com>
2015-07-07 17:34:41 -0400

    Add new snippet that shows how to display the event's featured image in SPCO.

---
+++ checkout/jf_ee_display_event_featured_image_on_spco.php
@@ -0,0 +1,22 @@
+<?php
+
+function jf_ee_display_event_featured_image_on_spco(){
+    $checkout = EE_Registry::instance()->SSN->checkout();
+    if ( $checkout instanceof EE_Checkout ) {
+        $transaction = $checkout->transaction;
+        if ( $transaction instanceof EE_Transaction ) {
+            foreach ( $transaction->registrations() as $registration ) {
+                if ( $registration instanceof EE_Registration ) {
+                    $event = $registration->event();
+                    if ( $event instanceof EE_Event ) {
+                        $events[ $event->ID() ] = $event;
+                        $image = $event->feature_image( 'full' );
+                    }
+                }
+            }   
+        }
+    }
+    echo $image;
+}
+
+add_action( 'AHEE__SPCO__before_registration_steps', 'jf_ee_display_event_featured_image_on_spco' );
