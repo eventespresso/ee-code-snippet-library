cfcd377ef9b6437ac11fdab31e93ca4bac1b814f
mnelson4 <michael@eventespresso.com>
2015-08-19 10:26:25 -0700

    just tell folks this works best for 4.7

--- admin/registration_report_add_price_modifiers.php
+++ admin/registration_report_add_price_modifiers.php
@@ -1,7 +1,8 @@
 <?php
 /**
- * Use this filter with EE4.6+ to add another column onto the registration CSV report
- * that shows the price modifiers for each registration's ticket purchased
+ * Use this filter with EE4.6-4.7 to add another column onto the registration CSV report
+ * that shows the price modifiers for each registration's ticket purchased.
+ * If you want to use with 4.8+ and have it account for promotions and transaction-wide surcgarges/discountds, please open a new ticket on this repo
  */
 
 add_filter( 'FHEE__EE_Export__report_registrations__reg_csv_array', 'espresso_add_ticket_base_price', 10, 2);
