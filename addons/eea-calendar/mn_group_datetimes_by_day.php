30482810c45be26ff860c1cd3eaf66a8853b3dd2
Mike Nelson <michael@eventespresso.com>
2015-10-06 16:29:31 -0700

    added code snippet from codebase ticket 8864

---
+++ addons/eea-calendar/mn_group_datetimes_by_day.php
@@ -0,0 +1,33 @@
+<?php
+//works with calendar 3.2.6.rc.002 or higher. Affects the month view of the calendar
+//so that if there are multiple datetimes for a single event on the same day, only one
+//will appear in the calendar. Week and day views are unaffected
+//see https://events.codebasehq.com/projects/event-espresso/tickets/8864
+add_filter( 'FHEE__EED_Espresso_Calendar__get_calendar_events__query_params', 'ee_calendar_group_by_day', 10, 7 );
+function ee_calendar_group_by_day( $query_params, 
+        $category_id_or_slug,
+        $venue_id_or_slug,
+        $public_event_stati,
+        $start_date,
+        $end_date,
+        $show_expired ) {
+    //only override month view
+    if( ( $end_date->getTimestamp() - $start_date->getTimestamp() ) <= WEEK_IN_SECONDS ) {
+        return $query_params;
+    }
+    //ok so it's month view. Let's issue a query grouped by event Id and date
+    $query_params[ 'group_by' ] = array( 'EVT_ID', 'event_date' );
+    $datetime_ids = EEM_Datetime::instance()->get_all_wpdb_results( 
+            $query_params, 
+            ARRAY_A, 
+            array( 
+                'DTT_ID' => array( 'Datetime.DTT_ID', '%d' ),
+                'EVT_ID' => array( 'Datetime.EVT_ID', '%d' ),
+                'event_date' => array( 'DATE( Datetime.DTT_EVT_start )', '%s' ) ) );
+    $ids_only = array_column( $datetime_ids, 'DTT_ID' );
+    //...and return query params so we only look for those specific datetimes
+    return array(
+        array( 
+            'DTT_ID' => array( 'IN', $ids_only ) ),
+            'limit' => count( $ids_only ) );
+}
