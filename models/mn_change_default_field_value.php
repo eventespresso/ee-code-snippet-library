d53d77454e1bce09576537edd6961cafadb661c7
Mike Nelson <michael@eventespresso.com>
2015-12-14 14:28:54 -0800

    [touch:9132]{t:20}add code snippet for changing model fields default value

---
+++ models/mn_change_default_field_value.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Changes the event's default value for displaying the ticket selector.
+ * Any model's fields' default values can be changed using similar code snippets
+ * @param mixed $default_value
+ * @param EE_Model_Field_Base $field_obj
+ */
+function ee_change_ticket_selector_default( $default_value, $field_obj ) {
+    if( $field_obj instanceof EE_Model_Field_Base 
+        && $field_obj->get_model_name() == 'Event' 
+        && $field_obj->get_name() == 'EVT_display_ticket_selector' ) {
+        return false;
+    }
+    return $default_value;
+}
+add_filter( 'FHEE__EE_Model_Field_Base___construct_finalize___default_value', 'ee_change_ticket_selector_default', 10, 2 );
