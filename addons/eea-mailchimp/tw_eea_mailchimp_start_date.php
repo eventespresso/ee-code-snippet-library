808f4e9eb878a043ea6b3c871f81e982ffdc44e8
Tony Warwick <tony@eventespresso.com>
2016-03-04 14:11:02 +0000

    Add function to send the next upcoming datetimes start_date to an MC List merge var valled EVENTDATE.

---
+++ addons/eea-mailchimp/tw_eea_mailchimp_start_date.php
@@ -0,0 +1,34 @@
+<?php
+//* Please do NOT include the opening php tag, except of course if you're starting with a blank file.
+
+//This function passes the next upcoming datetime start_date within the subscribe call to MailChimp.
+//You need to add a EVENTDATE merge var to your mailchimp list for this to work.
+
+function tw_eea_mailchimp_start_date( $subscribe_args, $registration, $EVT_ID ) {
+
+	//Check we have a valid EE Registration
+	if( $registration instanceof EE_Registration ) {
+		
+		//Pull the merge_vars array from $subscribe_args.
+		$merge_vars = $subscribe_args['merge_vars'];
+
+		//Pull the datetimes assigned to this ticket that have a start date greater than today.
+		$where = array( 'DTT_EVT_start' => array( '>=', current_time( 'mysql' ) ) );
+
+		$datetimes = $registration->ticket()->datetimes( array( $where,	'order_by'=> array('DTT_EVT_start' => 'ASC' ) ) );
+
+		//Use the first datetime.
+		$datetime = reset($datetimes);
+
+		//Add the 'EVENTDATE' merge var and use the datetimes start_date for the value.
+		$merge_vars['EVENTDATE'] = $datetime->start_date( 'd/m/Y' );
+
+		//Add the merge vars back into the subscriptions args.
+		$subscribe_args['merge_vars'] = $merge_vars;
+
+	}
+	
+	return $subscribe_args;
+
+}
+add_filter('FHEE__EE_MCI_Controller__mci_submit_to_mailchimp__subscribe_args', 'tw_eea_mailchimp_start_date', 10, 3);
