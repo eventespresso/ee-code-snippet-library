dc41fc5d0abecd9795bc6ac5916fcabc763cab6c
Tony Warwick <tony@eventespresso.com>
2016-05-12 09:58:07 +0100

    Add function that can be used to replace newlines within the CSV values with a single space.

---
+++ admin/tw_ee_swap_newlines_for_spaces_in_export.php
@@ -0,0 +1,12 @@
+<?php
+//* Please do NOT include the opening php tag, except of course if you're starting with a blank file
+
+//This function replaces any new lines within the CSV values for a single space.
+function tw_ee_replace_newlines_for_spaces_in_export($reg_csv_array, $reg_row) {
+	foreach( $reg_csv_array as $key => $value ) {
+		$clean_value = preg_replace("/[\r\n]+/", " ", $value);
+		$reg_csv_array[$key] = $clean_value;
+	}
+	return $reg_csv_array;
+}
+add_filter( 'FHEE__EE_Export__report_registrations__reg_csv_array', 'tw_ee_replace_newlines_for_spaces_in_export', 100, 2 );
