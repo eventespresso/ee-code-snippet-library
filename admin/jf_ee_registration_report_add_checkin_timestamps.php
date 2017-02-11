699723542234d8e41f61f2855d8b6987033bd2af
Josh Feck <josh@nxnweb.com>
2017-02-10 22:02:10 -0500

    Add snippet for adding Checkin Timestamp column to CSV Registration report.

---
+++ admin/jf_ee_registration_report_add_checkin_timestamps.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Use this filter with EE4.6+ to add another column onto the registration CSV report
+ * that shows checkin timestamps for each registration.
+ */
+
+add_filter( 'FHEE__EE_Export__report_registrations__reg_csv_array', 
+  'espresso_add_checkin_timestamp_csv_report', 
+  10, 
+  2
+);
+function espresso_add_checkin_timestamp_csv_report( array $csv_row, $reg_db_row ) {
+  $checkin_rows = (array)EEM_Checkin::instance()->get_all_wpdb_results(
+    array(
+      array(
+        'REG_ID' => $reg_db_row['Registration.REG_ID'],
+      ),
+    )
+  );
+  $checkins_for_csv_col = array();
+  foreach ( $checkin_rows as $checkin_row ) {
+      $checkins_for_csv_col[] = $checkin_row ['Checkin.CHK_timestamp'];
+  }
+  $csv_row[ (string)__( 'Checkin timestamps', 'event_espresso' ) ] = implode( ' + ', $checkins_for_csv_col );
+  return $csv_row;
+}
