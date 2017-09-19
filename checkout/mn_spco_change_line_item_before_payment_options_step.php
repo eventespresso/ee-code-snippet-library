ff291305e7bf3861a4e8684099bd608f38d3f99b
Mike Nelson <michael@eventespresso.com>
2017-09-19 11:11:11 -0700

    Update mn_spco_change_line_item_before_payment_options_step.php

--- checkout/mn_spco_change_line_item_before_payment_options_step.php
+++ checkout/mn_spco_change_line_item_before_payment_options_step.php
@@ -2,8 +2,8 @@
 //changes the first line item's unit price to 10, from whatever it was before
 //this will be displayed during SPCO's payment options step
 
-add_action('AHEE__Single_Page_Checkout__after_attendee_information__process_reg_step', 'custom_hook', 10, 1);
-function custom_hook(EE_SPCO_Reg_Step_Attendee_Information $data)
+add_action('AHEE__Single_Page_Checkout__after_attendee_information__process_reg_step', 'ee_spco_change_line_item_before_payment_options_step', 10, 1);
+function ee_spco_change_line_item_before_payment_options_step(EE_SPCO_Reg_Step_Attendee_Information $data)
 {
     $purchases = $data->checkout->transaction->items_purchased();
     $first_item = reset($purchases);
