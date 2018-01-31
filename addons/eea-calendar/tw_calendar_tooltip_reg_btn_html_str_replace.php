a891760fa00a8c408a2d6e167f1aa2c6db1a35c6
Tony Warwick <tony@eventespresso.com>
2018-01-31 10:03:30 +0000

    Add custom function used to change the calendar 'Register Now' text using str_replace.

---
+++ addons/eea-calendar/tw_calendar_tooltip_reg_btn_html_str_replace.php
@@ -0,0 +1,12 @@
+<?php
+//* Please do NOT include the opening php tag, except of course if you're starting with a blank file
+
+//This is an example of how you can change the register now button within the calendar using str_replace.
+//This function changes 'Register Now' to 'Custom Text'.
+function tw_ee_calendar_tooltip_reg_now_btn_str_replace($tooltip_reg_btn_html, $event, $datetime) {
+
+	$tooltip_reg_btn_html = str_replace('Register Now', 'Custom Text', $tooltip_reg_btn_html);
+
+	return $tooltip_reg_btn_html;
+}
+add_filter('FHEE__EE_Calendar__get_calendar_events__tooltip_reg_btn_html', 'tw_ee_calendar_tooltip_reg_now_btn', 10, 3);
