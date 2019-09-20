56f6dda45fbb0a97749be2f3bd75d6c2059f895e
Josh Feck <josh@nxnweb.com>
2019-09-20 13:09:42 -0400

    get out/bail if this is an admin request

--- addons/eea-wp-user/mn_wp_user_addon_save_custom_answers_too.php
+++ addons/eea-wp-user/mn_wp_user_addon_save_custom_answers_too.php
@@ -6,6 +6,9 @@
 add_filter('FHEE__EE_SPCO_Reg_Step_Attendee_Information___generate_question_input__input_constructor_args', 'my_question_input', 10, 4);
 function my_question_input( $input_args, EE_Registration $registration = null, EE_Question $question = null, EE_Answer $answer = null ) {
 	if( class_exists( 'EED_WP_Users_SPCO' ) ) {
+        if(is_admin()){
+            return $input_args;
+        }
 		global $current_user;
 		$attendee = EED_WP_Users_SPCO::get_attendee_for_user( $current_user );
 	} else {
