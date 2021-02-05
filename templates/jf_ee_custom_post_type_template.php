52f28e82d167c8212def237d751f4804287d4e32
Josh Feck <josh@nxnweb.com>
2021-02-05 09:08:15 -0500

    Update jf_ee_custom_post_type_template.php

--- templates/jf_ee_custom_post_type_template.php
+++ templates/jf_ee_custom_post_type_template.php
@@ -1,5 +1,7 @@
 <?php
 //* Please do NOT include the opening php tag, except of course if you're starting with a blank file
+    // change page.php (line 10) to match the name of the desired template
+    // if using a child theme, change get_template_directory() to get_stylesheet_directory()
 
 add_filter('single_template', 'jf_ee_custom_post_type_template');
 function jf_ee_custom_post_type_template($single_template) {
