cfd4f5820ec458a185419f6706db93e6575bafc3
joaovictorangeline <eujoaoangeline@gmail.com>
2022-05-18 21:51:56 -0300

    Filter to change the button text.

---
+++ templates/jv_change_register_now_button_text.php
@@ -0,0 +1,7 @@
+<?php
+// Remove the <?php tag if added to your functions.php
+// It changes the Register Now button string (Located on the Event page)
+add_filter( 'FHEE__EE_Ticket_Selector__display_ticket_selector_submit__btn_text', 'change_register_now_text' );
+function change_register_now_text() {
+	return 'Purchase now'; // Replace by your text
+}
