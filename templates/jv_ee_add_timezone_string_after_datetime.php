7016b4209ea823e10a3c7d3d116997be2142d74d
joaovictorangeline <eujoaoangeline@gmail.com>
2022-05-23 22:17:46 -0300

    Name standard applied to file and function.

--- templates/jv_ee_add_timezone_string_after_datetime.php
+++ templates/jv_ee_add_timezone_string_after_datetime.php
@@ -3,8 +3,8 @@
 // Remove the <?php tag if added to your functions.php
 // It allow you to add your timezone (i.e GMT+3) after the date time event information.
 // Replace the "your timezone string" by whatever you want.
-add_filter( 'FHEE__espresso_list_of_event_dates__datetime_html', 'jv_add_timezone_string_after_datetime', 10, 2 );
-function jv_add_timezone_string_after_datetime( $datetime_html, EE_Datetime $datetime ) {
+add_filter( 'FHEE__espresso_list_of_event_dates__datetime_html', 'jv_ee_add_timezone_string_after_datetime', 10, 2 );
+function jv_ee_add_timezone_string_after_datetime( $datetime_html, EE_Datetime $datetime ) {
 	$date_format = get_option( 'date_format' );
 	$time_format = get_option( 'time_format' );
 	return '
