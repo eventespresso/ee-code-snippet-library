ffcb2db1c0093124100e349605221c2021da3370
joaovictorangeline <eujoaoangeline@gmail.com>
2022-05-23 22:44:40 -0300

    File and function name following the standards.

--- templates/jv_ee_change_register_now_text.php
+++ templates/jv_ee_change_register_now_text.php
@@ -1,7 +1,7 @@
 <?php
 // Remove the <?php tag if added to your functions.php
 // It changes the Register Now button string (Located on the Event page)
-add_filter( 'FHEE__EE_Ticket_Selector__display_ticket_selector_submit__btn_text', 'jv_change_register_now_text' );
-function jv_change_register_now_text() {
+add_filter( 'FHEE__EE_Ticket_Selector__display_ticket_selector_submit__btn_text', 'jv_ee_change_register_now_text' );
+function jv_ee_change_register_now_text() {
 	return 'Purchase now'; // Replace by your text
 }
