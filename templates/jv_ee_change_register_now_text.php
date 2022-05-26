b0c9e95b58cc98140deed19bc7f77f921070fccd
joaovictorangeline <eujoaoangeline@gmail.com>
2022-05-26 14:06:00 -0300

    Space added.

--- templates/jv_ee_change_register_now_text.php
+++ templates/jv_ee_change_register_now_text.php
@@ -3,5 +3,7 @@
 // It changes the Register Now button string (Located on the Event page)
 add_filter( 'FHEE__EE_Ticket_Selector__display_ticket_selector_submit__btn_text', 'jv_ee_change_register_now_text' );
 function jv_ee_change_register_now_text() {
+
 	return 'Purchase now'; // Replace by your text
+	
 }
